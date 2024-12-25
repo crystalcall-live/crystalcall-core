31bb400159610288c47e6398a0e87f655b79b2f8
Manasseh <believemanasseh@gmail.com>
2024-12-25 16:40:27 +0100

    docs: add swagger docs

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,11 +1,16 @@
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.io.File
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        get("") {
+            call.respondFile(File("src/main/resources/openapi/swagger/index.html"))
+        }
+
+        get("/documentation.yaml") {
+            call.respondFile(File("src/main/resources/openapi/documentation.yaml"))
         }
     }
 }
