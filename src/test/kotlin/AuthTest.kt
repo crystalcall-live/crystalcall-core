0d3504a01efc1c6d2469d87d9084b3605d302204
Manasseh <believemanasseh@gmail.com>
2025-01-03 15:43:21 +0100

    test: fix auth test

--- src/test/kotlin/AuthTest.kt
+++ src/test/kotlin/AuthTest.kt
@@ -1,5 +1,5 @@
-import dtos.AuthResponse
 import dtos.LoginDTO
+import dtos.Response
 import dtos.SignupDTO
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
@@ -10,18 +10,24 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class AuthTest {
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
+
     @Test
     fun testLogin() = testApplication {
         application {
             module(testing = true)
         }
+
         val response = client.post("/v1/login") {
             header(HttpHeaders.ContentType, ContentType.Application.Json)
             setBody(Json.encodeToString(LoginDTO.serializer(), LoginDTO("test@email.com", "password")))
         }
-        val jsonResponse = Json.decodeFromString<AuthResponse>(response.bodyAsText())
+        println(response.bodyAsText())
+        val jsonResponse = json.decodeFromString<Response.AuthResponse>(response.bodyAsText())
         assertEquals(HttpStatusCode.OK, response.status)
-        assertEquals("test@email.com", jsonResponse.data?.email)
+        assertEquals("test@email.com", jsonResponse.data.email)
     }
 
     @Test
@@ -29,6 +35,7 @@ class AuthTest {
         application {
             module(testing = true)
         }
+
         val response = client.post("/v1/signup") {
             header(HttpHeaders.ContentType, ContentType.Application.Json)
             setBody(
@@ -38,9 +45,9 @@ class AuthTest {
                 )
             )
         }
-        val jsonResponse = Json.decodeFromString<AuthResponse>(response.bodyAsText())
+        val jsonResponse = json.decodeFromString<Response.AuthResponse>(response.bodyAsText())
         assertEquals(HttpStatusCode.Created, response.status)
-        assertEquals("test1@email.com", jsonResponse.data?.email)
+        assertEquals("test1@email.com", jsonResponse.data.email)
     }
 
 }
