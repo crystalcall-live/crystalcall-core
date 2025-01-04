fdf6c94aba031adb297948ce83c06d130105568a
Manasseh <believemanasseh@gmail.com>
2025-01-04 10:23:29 +0100

    feat: allow hosts in cors config

--- src/main/kotlin/HTTP.kt
+++ src/main/kotlin/HTTP.kt
@@ -1,11 +1,11 @@
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.plugins.swagger.*
-import io.ktor.server.routing.*
 
 fun Application.configureHTTP() {
     install(CORS) {
+        allowHost("localhost:5173")
+        allowHost("crystalcall.daimones.xyz")
         allowMethod(HttpMethod.Options)
         allowMethod(HttpMethod.Put)
         allowMethod(HttpMethod.Delete)
