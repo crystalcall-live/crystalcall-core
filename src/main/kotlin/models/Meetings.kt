1b9c33bf3e3a423e76257ffd5787bccdaa459133
Manasseh <believemanasseh@gmail.com>
2025-01-08 11:28:20 +0100

    feat: use exposed's dao api

--- src/main/kotlin/models/Meetings.kt
+++ src/main/kotlin/models/Meetings.kt
@@ -1,14 +1,29 @@
 package models
 
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.javatime.datetime
 import java.time.LocalDateTime
 
 object Meetings : IntIdTable() {
-    val link: Column<String> = varchar("link", 255)
+    val link: Column<String> = varchar("link", 200)
     val isExpired: Column<Boolean> = bool("is_expired").default(false)
     val created: Column<LocalDateTime> = datetime("created").default(LocalDateTime.now())
     val modified: Column<LocalDateTime> = datetime("modified").default(LocalDateTime.now())
-    val user = reference("users", Users)
+    val owner = reference("owner", Users, onDelete = ReferenceOption.CASCADE)
+}
+
+class Meeting(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<Meeting>(Meetings)
+
+    val link by Meetings.link
+    val isExpired by Meetings.isExpired
+    val created by Meetings.created
+    val modified by Meetings.modified
+    val owner by User referencedOn Meetings.owner
+    val participants by User optionalReferrersOn Users.currentMeeting
 }
--- src/main/kotlin/models/Users.kt
+++ src/main/kotlin/models/Users.kt
@@ -1,5 +1,8 @@
 package models
 
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.javatime.datetime
@@ -14,4 +17,18 @@ object Users : IntIdTable() {
     val isActive: Column<Boolean> = bool("is_active").default(false)
     val created: Column<LocalDateTime> = datetime("created").default(LocalDateTime.now())
     val modified: Column<LocalDateTime> = datetime("modified").default(LocalDateTime.now())
+    val currentMeeting = optReference("current_meeting", Meetings)
+}
+
+class User(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<User>(Users)
+
+    var email by Users.email
+    var firstName by Users.firstName
+    var lastName by Users.lastName
+    var password by Users.password
+    var isActive by Users.isActive
+    var created by Users.created
+    var modified by Users.modified
+    var currentMeeting by Meeting optionalReferencedOn Users.currentMeeting
 }
